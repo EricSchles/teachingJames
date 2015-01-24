a7f5fbfd63eb41974104b46d3eba55654b0b1060
EricSchles <ericschles@gmail.com>
2015-01-23 17:22:37 -0800

    yea

--- python_tutorial_one.py
+++ python_tutorial_one.py
@@ -1,1 +1,11 @@
-print "Hello world"
+#print "Hello world"
+#print "James, you are a programmer guy, now."
+
+#this program prints hello james.
+#In case you forgot what happens...
+x = "Hello, James"
+#print x
+
+y = [x,"stuff",18,["yes","i","am","doing","this"]]
+for element in y:
+    print element
